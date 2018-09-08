7fa25769f5b922cee8b407f80bfab438cf0b6112
kaien <kaienkira@gmail.com>
2018-09-08 17:30:10 +0800

    fix cpp BaseStruct::dumpBytes() bug

--- cpp/src/brickred/exchange/base_struct.cc
+++ cpp/src/brickred/exchange/base_struct.cc
@@ -20,12 +20,12 @@ std::string BaseStruct::dumpBytes(const std::string &val)
         return "";
     }
 
-    std::vector<char> ret(val.size() * 3);
+    std::vector<char> ret(val.size() * 3 + 1);
 
     for (size_t i = 0; i < val.size(); ++i) {
         ::snprintf(&ret[i * 3], 4, "%02hhX-", val[i]);
     }
-    ret.back() = '\0';
+    ret[ret.size() - 2] = '\0';
 
     return std::string(&ret[0]);
 }
